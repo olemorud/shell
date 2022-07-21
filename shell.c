4f370ef001d1d7248c933e39270ad464c2923a7b
Ole Morud <olekr99@hotmail.com>
2022-07-21 23:59:03 +0200

    add minimum viable shell

---
+++ shell.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+#define MAXLEN 1000
+#define _str(x) #x
+#define str(X) _str(X)
+
+void clear_buffer(){
+	int c;
+	while ((c = getchar()) != '\n' && c != EOF) { }
+}
+
+void read_arglist(char **args){
+	char *token, 
+		 input[MAXLEN],
+		 saveptr[MAXLEN];
+
+	printf("\n\n>");
+	scanf("%" str(MAXLEN) "[^\n]", input);
+	clear_buffer();
+
+	int i;
+	token = strtok_r(input, " ", (char**)&saveptr);
+
+	for(i=0; token; i++) {
+		args[i] = malloc( (1+strlen(token)) * sizeof(char) );
+		strcpy(args[i], token);
+
+		token = strtok_r(NULL, " ", (char**)&saveptr);
+	}
+	args[i] = (char*)NULL;
+}
+
+void free_arglist(char **args){
+	int i=0;
+	char **ptr = args;
+
+	while(*ptr != NULL){
+		free(*ptr);
+		ptr++;
+	}
+
+	strcpy(args[0], "");
+}
+
+int main(){
+	char **args = malloc(MAXLEN/2);
+
+	while(1){
+		read_arglist(args);
+
+		int pid = fork();
+		switch( pid ){
+			case 0: /* child */
+				execvp(args[0], args);
+				printf("%s\n", strerror(errno));
+				exit(1);
+				break;
+			case -1: /* error */
+				printf("fork failed");
+				break;
+		}
+		
+		/* parent */
+		wait(NULL);
+		free_arglist(args);
+	}
+
+	return 0;
+}
